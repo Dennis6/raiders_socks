11a735ab0f138c8be351fecb5e323ae48d6abcc5
Dennis6 <DJ@Denniss-MacBook-Pro.local>
2021-06-04 23:25:00 -0400

    Updated conditional rendering

--- main.js
+++ main.js
@@ -31,11 +31,28 @@ NOTES:
 :class="expression"
 :style="isStyled"
 :disabled="isDisabled"
+
+Conditional Rendering
+- v-if=" " evaluates if the conditon is true 
+- v-else
+- v-else-if
+- We can combine the v-if directive with its sister directive v-else to display another element as the fallback if the first condition turns out to be falsey.
+- It’s worth noting that you don’t always need to pair v-if with v-else. There are plenty of use cases where you don’t need a fallback element to render. However, in these cases, it is sometimes a better option to use the v-show directive.
+- The v-show directive is used for toggling an element’s visibility instead of adding and removing the element from the DOM entirely, like v-if does.
+- 
+<p v-if="inventory > 10">In Stock</p>
+<p v-else-if="inventory <= 10 && inventory > 0">Almost sold out</p>
+<p v-else>Out of Stock</p>
+- if the expression in the quotes evaluates to a truthy value, the element will display.
+- v-show=" " display when data is true and not display if false 
+
+LIST RENDERING:
+<div v-for="variant in variants" :key="variant.id">{{ variant.color }}</div>
+- using dot notation to print out each variant as we loop through the variants array.
+Key Attribute: An essential for list items
+By saying :key="variant.id", we’re using the shorthand for v-bind to bind the variant’s id to the key attribute. This gives each DOM element a unique key so that Vue can grasp onto the element and not lose track of it as things update within the app.
+This provides some performance improvements, and later down the line, if you’re doing something like animating your elements, you’ll find that the key attribute really helps Vue effectively manage your elements as they move around the DOM.
  */
-
-//(1) Add a description to the data object with the value "A pair of warm, fuzzy socks". Then display the description using an expression in an p element, underneath the h1.
-
-//(2) Add a link to your data object, and use v-bind to sync it up with an anchor tag in your HTML. Hint: you’ll be binding to the href attribute.
 
 var app = new Vue({
   el: "#app",
@@ -44,13 +61,26 @@ var app = new Vue({
     description: "A pair of warm, fuzzy socks",
     image: "img/raider_socks.jpeg",
     link: "https://www.raiders.com/",
+    inStock: "true",
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "sliver",
+      },
+      {
+        variantId: 2235,
+        variantColor: "black",
+      },
+    ],
+    price: "100",
+    sizes: ["S", "M", "L", "XL", "XXL"],
   },
 });
 
 /*
 NOTES:
-Conditional Rendering
-- v-if=" " evaluates if the conditon is true 
-- v-else
+EVENT HANDLING:
+
 
 */
